fc870c88cc04f1caa9dd0031fe764f0c899ec07a
Mohamed <mohamed>
2023-12-25 01:43:04 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Funktion für ein einfaches Begrüßungsbanner
+function showWelcomeBanner() {
+    alert("Willkommen auf meiner Webseite!");
+}
+
+// Funktion zum Ändern des Hintergrundbilds bei Mausklick
+function changeBackground() {
+    document.body.style.backgroundImage = "url('neues-hintergrundbild.jpg')";
+}
+
+// Du kannst weitere Funktionen hinzufügen, je nach den Bedürfnissen deiner Webseite
